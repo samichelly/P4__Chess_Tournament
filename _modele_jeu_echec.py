eaef74aba1e6ca012679a35709fff2392544a292
Sami Chelly <samichelly15@gmail.com>
2023-05-04 18:56:50 +0200

    Creation class Joueur

---
+++ _modele_jeu_echec.py
@@ -0,0 +1,52 @@
+class joueur:
+    def __init__(self, idNatEchec, nomDeFamille, prenom, dateDeNaissance, score_total):
+         self.idNatEchec = idNatEchec
+         self.nomDeFamille = nomDeFamille
+         self.prenom = prenom
+         self.dateDeNaissance = dateDeNaissance
+         self.score_total = score_total
+
+def match_J1_vs_J2(J1, J2):
+    resultat_J1 = resultat_J2 = 0
+    # générateur de score aléatoire
+    resultat_J1 = 1
+    resultat_J2 = 0
+    if resultat_J1 == 1:
+        print("J1 a gagné")
+    elif resultat_J1 == resultat_J2:
+        print("J1 et J2 ont fait match nul")
+    else:
+        print("J2 a gagné")
+    return resultat_J1, resultat_J2
+
+class match:
+    def __init__(self,paire):
+        self.paire = paire
+
+    def opposition(self, paire):
+        J1 = paire[0]
+        J2 = paire[1]
+        match_J1_vs_J2(J1, J2)
+        
+        
+
+joueur1 = joueur("AAAAAAA", "AAAAAAA", "AAAAAAA", "AAAAAAA", 1)
+joueur2 = joueur("AAABBBB", "AAABBBB", "AAABBBB", "AAABBBB", 0)
+
+print(joueur1.prenom)
+print(joueur2)
+
+# class tournoi:
+#     def __init__(self) -> None:
+
+
+# class tours:
+#     def __init__(self, 	idTours,	Nom,	dateHeureDeDebut,	dateHeureDeFin,	listMatch) -> None:
+# 		#(ex : round X)
+
+
+
+
+
+
+
