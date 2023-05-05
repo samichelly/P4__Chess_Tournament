5e649d91ab64b7a703a46c98b0acc22afa8884fc
Sami Chelly <samichelly15@gmail.com>
2023-05-05 14:45:00 +0200

    Ajout de la methode getListJoueur dans la class Tournoi

--- _modele_jeu_echec.py
+++ _modele_jeu_echec.py
@@ -1,18 +1,22 @@
 import pandas as pd
 from datetime import date
+from pprint import pprint
 
 class joueur:
-    def __init__(self, idNatEchec, nomDeFamille, prenom, dateDeNaissance, score_cumule=0):    # score_cumule=0
+    def __init__(self, idNatEchec, nomDeFamille, prenom, dateDeNaissance, classement):    # score_cumule=0
          self.idNatEchec = idNatEchec
          self.nomDeFamille = nomDeFamille
          self.prenom = prenom
          self.dateDeNaissance = dateDeNaissance
-         self.score_cumule = score_cumule
+         self.classement = classement
     
+    def listedesjoueurs(self, idNatEchec, nomDeFamille, prenom):
+        print(idNatEchec, nomDeFamille, prenom)
+        return idNatEchec
     # def score_Joueurs(self, ):
 
 class tournoi:
-    def __init__(self, nom, lieu, dateDeDebut, dateDeFin, nbTours, nTourActuel):#, listeTour, listeJoueursEnregistres, description):
+    def __init__(self, nom, lieu, dateDeDebut, dateDeFin, nbTours, nTourActuel, listeTour, listeJoueursEnregistres, description):
         self.nom = nom
         self.lieu = lieu
         self.dateDeDebut = dateDeDebut
@@ -23,13 +27,16 @@ class tournoi:
         # self.listeJoueursEnregistres = listeJoueursEnregistres
         # self.description = description
     
-    def informations_tournoi(self, nom, lieu, dateDeDebut, dateDeFin, nbTours, nTourActuel):
-        print(f"Tournoi {nom} à {lieu}, commence le {dateDeDebut} et se termine {dateDeFin}. Le tournoi est composé de {nbTours}. Tour actuel {nTourActuel}")
+    def informations_tournoi(self, nom, lieu, dateDeDebut, dateDeFin, nbTours, nTourActuel, listeJoueursEnregistres):
+        print(f"Tournoi {nom} à {lieu}, commence le {dateDeDebut} et se termine {dateDeFin}. Le tournoi est composé de {nbTours}. Tour actuel {nTourActuel}.")
+    # def etablir_classement(self, nbTours, nTourActuel):  #ajouter joueur sous forme de dictionnaire
+    #     if nbTours == nTourActuel:
+    #         print("Le vainqueur est ")
     
-    def etablir_classement(self, nbTours, nTourActuel):  #ajouter joueur sous forme de dictionnaire
-        if nbTours == nTourActuel:
-            print("Le vainqueur est ")
-            
+    def getListJoueur(self, listeJoueursEnregistres):
+        print(f"Liste des participants : {listeJoueursEnregistres}")
+        return listeJoueursEnregistres
+
     def commencer_tour(self):
     # def nbToursTournoi(self, nbTours):
         pass
@@ -42,7 +49,9 @@ class tours:
         self.dateHeureDeDebut = dateHeureDeDebut
         self.dateHeureDeFin = dateHeureDeFin
         self.listMatch = listMatch
-    
+
+    def liste_des_joueurs(self, listeParticipants):
+        pass
     def generation_des_paires():
         pass
 		
@@ -72,9 +81,19 @@ class match:
         match_J1_vs_J2(J1, J2)
         
 
-# joueurs = {}
-# for _ in joueurs:
-#     joueur1 = joueur("AAAAAAA", "AAAAAAA", "AAAAAAA", "AAAAAAA")
+
+
+
+# for i in range(1, 8):
+#     joueurs = [joueur.listedesjoueurs(i, i , "AAAAAAA", "AAAAAAA")]
+
+joueurs = [joueur.listedesjoueurs(i, i , "AAAAAAA", "AAAAAAA") for i in range(1, 8)]
+nombre_tour = input("Entrez le nombre de tours souhaitez pour ce tournoi :")
+tournoi.informations_tournoi("Premier Tournoi", "Marseille", date.today(), "samedi", nombre_tour, 2, 6, joueurs)
+
+# Listes_Joueurs_tournoi = joueur.listedesjoueurs()
+Listes_Joueurs = tournoi.getListJoueur(joueurs, joueurs)
+
 
 # test_joueur = joueur("AAAAAAA", "AAAAAAA", "AAAAAAA", "AAAAAAA")
 # col = ("col1", "col2", "col3", "col4")
@@ -82,15 +101,15 @@ class match:
 # print(test_jou)
 
 
-joueur1 = joueur("AAAAAAA", "AAAAAAA", "AAAAAAA", "AAAAAAA")
-joueur2 = joueur("AAABBBB", "AAABBBB", "AAABBBB", "AAABBBB")
-nombre_tour = input("Entrez le nombre de tours souhaitez pour ce tournoi :")
-tournoi.informations_tournoi("Premier Tournoi", "Marseille", date.today(), "samedi", nombre_tour, 2, 6)
+# joueur1 = joueur("AAAAAAA", "AAAAAAA", "AAAAAAA", "AAAAAAA")
+# joueur2 = joueur("AAABBBB", "AAABBBB", "AAABBBB", "AAABBBB")
+# nombre_tour = input("Entrez le nombre de tours souhaitez pour ce tournoi :")
+# tournoi.informations_tournoi("Premier Tournoi", "Marseille", date.today(), "samedi", nombre_tour, 2, 6)
 
 
-test_paire = [joueur1, joueur2]
-match1 = match(test_paire)
-match1.opposition(test_paire)
+# test_paire = [joueur1, joueur2]
+# match1 = match(test_paire)
+# match1.opposition(test_paire)
 
 # print(joueur1.prenom)
 # print(joueur2)
